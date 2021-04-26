4fd7a6fbee04878283aa2bb69d37dc8383218525
weekend79 <mortenviken@yahoo.com>
2021-04-26 22:02:28 +0000

    Updated settings file

--- syvfit/settings.py
+++ syvfit/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=e9(kqy(*2=#7ej5q3@x!y+g#!)p*!aj)k)9ry^2qz6xara9ha'
+
+SECRET_KEY = os.environ.get('SYVFIT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
